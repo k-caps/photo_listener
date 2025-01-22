a29a3c21c1b7f5afbf71478f121772527b29028b
Kobi <18639419+k-caps@users.noreply.github.com>
2025-01-22 23:43:13 +0200

    whitespace trim

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:provider/provider.dart';
 import 'logic/app_state.dart';
 import 'ui/main_screen.dart';
 
-
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(PhotoListener());
